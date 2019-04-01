86538848723f13e5461357534fe29f87daefc56c
tyrdom <tyrdom@hotmail.com>
2019-04-01 18:38:39 +0800

    gameplay

--- src/main/java/gameplayLib/Card.scala
+++ src/main/java/gameplayLib/Card.scala
@@ -36,17 +36,27 @@ case class SkillCard(id: Int, standard: CommonCard, owner: String, skills: Skill
 
 case class Skill(tricks: Seq[Trick], effects: Seq[CardEffect])
 
+case class Shape(spawnType: SpawnType, firstPoint: Int, num: Int)
+
 object Card {
 
 
   def genLinkTuple(card: Card): Option[(Int, Int, Int)] = card match {
     case commonCard: CommonCard => Some(commonCard.nowPoint - 1, commonCard.nowPoint, commonCard.nowPoint + 1)
-    case skillCard: SkillCard => {
+    case skillCard: SkillCard =>
       val point = skillCard.standard.nowPoint
       Some(point - 1, point, point + 1)
-    }
+
     case _ => None
 
+  }
+
+  def sortCard(Cards: Seq[Card]): Seq[Card] = Cards.sortWith(compareCardLessThan)
+
+
+  def getId(card: Card): Option[Int] = card match {
+    case commonCard: CommonCard => Some(commonCard.id)
+    case skillCard: SkillCard => Some(skillCard.id)
   }
 
   def getPoint(card: Card): Option[Int] = card match {
@@ -55,12 +65,19 @@ object Card {
     case _ => None
   }
 
+  def compareCardLessThan(a: Card, b: Card): Boolean = (a, b) match {
+    case (x, _) if getPoint(x).isEmpty => true
+    case (_, y) if getPoint(y).isEmpty => false
+    case (x, y) => getPoint(x).get < getPoint(y).get || (getPoint(x).get == getPoint(y).get && getId(x).get < getId(y).get)
+    case _ => true
+  }
 
-  def order(cards: Seq[Card]): Seq[Card] = ??? //TODO
+  //
+  //  def order(cards: Seq[Card]): Seq[Card] =
 
-  def shape(cards: Seq[Card]): (SpawnType, Int) = {
+  def shape(cards: Seq[Card]): Shape = {
 
 
-    (SpawnType.NoShape, 7)
+    Shape(SpawnType.NoShape, 7, 6)
   }
 }
