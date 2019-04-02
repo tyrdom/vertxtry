f02c290f2d3602db0cd44f99fff6c12ce73fd3ed
tyrdom <tyrdom@hotmail.com>
2019-04-02 16:39:32 +0800

    gameplay

--- src/main/java/gameplayLib/Card.scala
+++ src/main/java/gameplayLib/Card.scala
@@ -1,22 +1,19 @@
 package gameplayLib
 
-import gameplayLib.SpawnType.SpawnType
+import gameplayLib.Type.SpawnType
 import gameplayLib.Trick.Trick
-
-trait Card {
-
-}
 
 
 object Trick extends Enumeration {
   type Trick = Value
   val Hold = Value
-  val Cast = Value
+  val CastWith = Value
+  val AnyCast = Value
   val ThisCast = Value
   val OtherCast = Value
 }
 
-object SpawnType extends Enumeration {
+object Type extends Enumeration {
   type SpawnType = Value
   val Straight = Value
   val Multi = Value
@@ -30,54 +27,64 @@ class CardEffect {
 
 }
 
-case class CommonCard(id: Int, level: Int, nowPoint: Int, copy: Boolean) extends Card
-
-case class SkillCard(id: Int, standard: CommonCard, owner: String, skills: Skill) extends Card
+case class CommonValue(id: Int, level: Int, nowPoint: Int, copy: Boolean)
 
 case class Skill(tricks: Seq[Trick], effects: Seq[CardEffect])
 
-case class Shape(spawnType: SpawnType, firstPoint: Int, num: Int)
+case class Shape(keyPoint: Int, height: Int, length: Int, extraNum: Int, fillBlankRestNum: Int)
 
-object Card {
+case class Card(standard: CommonValue, owner: Option[String], skill: Option[Skill]) {
 
-
-  def genLinkTuple(card: Card): Option[(Int, Int, Int)] = card match {
-    case commonCard: CommonCard => Some(commonCard.nowPoint - 1, commonCard.nowPoint, commonCard.nowPoint + 1)
-    case skillCard: SkillCard =>
-      val point = skillCard.standard.nowPoint
-      Some(point - 1, point, point + 1)
-
-    case _ => None
-
-  }
 
   def sortCard(Cards: Seq[Card]): Seq[Card] = Cards.sortWith(compareCardLessThan)
 
+  def genPointMap(Cards: Seq[Card]): Map[Int, Int] = {
+    var map: Map[Int, Int] = Map()
+    val min = Config.minPoint - 1
+    val max = Config.maxPoint + 1
+    for (i <- min to max) {
+      if (i == min) {
+        map += (min -> Cards.count(x => x.standard.nowPoint <= min))
+      }
+      else if (i == max) {
+        map += (max -> Cards.count(x => x.standard.nowPoint >= max))
+      }
+      else
 
-  def getId(card: Card): Option[Int] = card match {
-    case commonCard: CommonCard => Some(commonCard.id)
-    case skillCard: SkillCard => Some(skillCard.id)
+        map += (max -> Cards.count(x => x.standard.nowPoint == i))
+
+    }
+    map
   }
 
-  def getPoint(card: Card): Option[Int] = card match {
-    case commonCard: CommonCard => Some(commonCard.nowPoint)
-    case skillCard: SkillCard => Some(skillCard.standard.nowPoint)
-    case _ => None
+  def compareCardLessThan(a: Card, b: Card): Boolean = a.standard.nowPoint < b.standard.nowPoint || (a.standard.nowPoint == b.standard.nowPoint && a.standard.id == b.standard.id)
+
+  def genShapes(cards: Seq[Card]): Seq[Shape] = {
+    val pointMap = genPointMap(cards)
+
+    Shape(1, 1, 1, 1, 1) +: Nil
   }
 
-  def compareCardLessThan(a: Card, b: Card): Boolean = (a, b) match {
-    case (x, _) if getPoint(x).isEmpty => true
-    case (_, y) if getPoint(y).isEmpty => false
-    case (x, y) => getPoint(x).get < getPoint(y).get || (getPoint(x).get == getPoint(y).get && getId(x).get < getId(y).get)
-    case _ => true
+  def canShapeCounter(cards: Seq[Card], shape: Shape): Option[Shape] = ???
+
+  //  def genMultiS(key: Int, usePoint: Seq[Int], temp: Seq[Shape]): Seq[Shape] = key match {
+  //    case Config.maxPoint => Shape(Type.Multi, Config.maxPoint, usePoint.count(_ == Config.maxPoint), usePoint.count(_ != Config.maxPoint), 0) +: temp
+  //    case nowKey =>
+  //      val nextTemp = Shape(Type.Multi, nowKey, usePoint.count(_ == nowKey), usePoint.count(_ != nowKey), 0) +: temp
+  //      genMultiS(key + 1, usePoint, nextTemp)
+  //  }
+
+
+  def genStraightS(usePoints: Seq[Int], temp: Seq[Shape], fillBlankNum: Int): Seq[Shape] = ???
+
+  def genAStraight(nowPoint: Int, restPoints: Seq[Int], fillBlank: Int, nowShape: Shape): Shape = {
+    def straightNextPoint(x: Int) = x match {
+      case Config.maxPoint => Config.minPoint
+      case xx => xx + 1
+    }
+
+    val nextPoint = straightNextPoint(nowPoint)
+
   }
 
-  //
-  //  def order(cards: Seq[Card]): Seq[Card] =
-
-  def shape(cards: Seq[Card]): Shape = {
-
-
-    Shape(SpawnType.NoShape, 7, 6)
-  }
 }
---
+++ src/main/java/gameplayLib/Config.scala
@@ -0,0 +1,7 @@
+package gameplayLib
+
+object Config {
+    val maxPoint =10
+    val minPoint =1
+    val minStraightNeed = 5
+}
