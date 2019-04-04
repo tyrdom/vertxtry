6794cfb71eaec7d20213fdd118002984eee5860b
tyrdom <tyrdom@hotmail.com>
2019-04-04 18:06:15 +0800

    gameplay

--- src/main/java/gameplayLib/Character.scala
+++ src/main/java/gameplayLib/Character.scala
@@ -1,11 +1,14 @@
 package gameplayLib
 
-case class Character(id: Int, name: String, var level: Int, var atk: Int, var defence: Int, var exp: Int) //也是有状态的
+case class Character(id: Int, name: String, var level: Int, var atk: Int, var defence: Int, var exp: Int) //有状态的数据，这个
 {
-  def addExp(amount: Int): Unit = {
+
+  def initCharacter(id:Int) :Character = ???
+
+  def addExp(amount: Int): Unit = {   //必须用此方法增加经验，才能触发级别改变
     this.exp = exp + amount
     this.level = foundLevel(exp)
-  } //必须用此方法增加经验，才能触发级别改变
+  }
 
   def foundLevel(exp: Int): Int = Config.levelArray.filter(x => x._1 <= exp).maxBy(_._2)._2
 
